1ce975c5d6d979f93436206aeee8ddf212f1b023
Areson251 <unicorn5596@gmail.com>
2023-07-28 16:10:40 +0400

    refactor

--- metrics.py
+++ metrics.py
@@ -1,15 +1,26 @@
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
+import argparse
 
-annType = 'bbox'
+def load_files(annFile, resFile):
+    cocoGT = COCO(annFile)
+    cocoDT = cocoGT.loadRes(resFile)
+    return cocoGT, cocoDT
 
-cocoGt=COCO(r'C:\Users\areson\Desktop\Разное\AIRI_2023\проект\restricted_result.json')
-cocoDt=cocoGt.loadRes(r'C:\Users\areson\Desktop\Разное\AIRI_2023\проект\owl-vit_result2.json')
+def calculate_metrics(annFile, resFile):
+    cocoGT, cocoDT = load_files(annFile, resFile)
+    
+    annType = ['bbox', 'segm']
+    
+    for ann in annType:
+        cocoEval = COCOeval(cocoGT, cocoDT, ann)
+        cocoEval.evaluate()
+        cocoEval.accumulate()
+        cocoEval.summarize()
 
-imgIds=sorted(cocoGt.getImgIds())
-
-cocoEval = COCOeval(cocoGt,cocoDt, annType)
-cocoEval.params.imgIds = imgIds
-cocoEval.evaluate()
-cocoEval.accumulate()
-cocoEval.summarize()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_gt", help="add path to gt")
+    parser.add_argument("path_to_result", help="add path to result")
+    args = parser.parse_args()
+    calculate_metrics(args.path_to_gt, args.path_to_result)
